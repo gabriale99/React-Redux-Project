f79e61e23f150701f47fd656d1ef1013613de9de
Gabriale99 <gabrielchung1216@gmail.com>
2019-08-08 04:24:23 -0700

    Updates

---
+++ precache-manifest.9d315935707db29d812a4522eab8dd53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d17be38bb21602cc8a07d8c176076d",
+    "url": "/React-Redux-Project/index.html"
+  },
+  {
+    "revision": "37f4cd2046b64bb24c55",
+    "url": "/React-Redux-Project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/React-Redux-Project/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "37f4cd2046b64bb24c55",
+    "url": "/React-Redux-Project/static/js/main.dd1b197d.chunk.js"
+  },
+  {
+    "revision": "7f43664544da6070397f",
+    "url": "/React-Redux-Project/static/js/runtime~main.ff12ee47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Redux-Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
