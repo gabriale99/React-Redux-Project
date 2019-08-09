2551d74797e2aff98c41b2e79ee5ef8b51ff5009
gabriale99 <gabrielchung1216@gmail.com>
2019-08-08 19:21:30 -0700

    Working on React JS 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Icons, Table, Button } from '../'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const Icon = Icons.AccessAlarm;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* <Icon />
+        <Button variant="contained" color="primary">
+          Primary
+        </Button> */}
+        {/*<Table… /> see https://material-ui.com/demos/tables/*/}
+      </div>
+    )
+  }
 }
 
 export default App;
